336af647676b6656573199c845c42ffb2aa4805d
Tharun Atithya <tharunaadhi6@gmail.com>
2025-09-11 09:39:52 +0530

    modeling using inheritance

--- Unit-2 Schema Design Patterns/inheritance.mongodb.js
+++ Unit-2 Schema Design Patterns/inheritance.mongodb.js
@@ -30,7 +30,78 @@ var first_pipeline = [
 
 db.books_collection.aggregate(first_pipeline);
 
-var specifi_
+// using variables for better readability
+var find_audio_books = {
+  $match:{
+    $and: [
+      {product_type: "unspecified"},
+      {audio_format: {$exists: true}}
+    ]
+  }
+}
+
+var redefine_producttype_audiobook = {
+  $set: {product_type: "audiobook"}
+}
+
+var merge_stage_audiobook = {
+  $merge: {
+    into: "books_collection",
+    on: "_id",
+    whenMatched: "replace",
+    whenNotMatched: "discard"
+  }
+}
+
+db.books_collection.aggregate([
+  find_audio_books, 
+  redefine_producttype_audiobook, 
+  merge_stage_audiobook
+]);
+
+// pipeline for ebook
+
+var ebook_and_physicalbook = {
+  $set:{
+    product_type: {
+      $switch:{
+        branches: [
+          {
+            case: {
+              $and: [
+                {$eq: ["$product_type", "unspecified"]}, 
+                {$ne:["$drm_protected", null]}
+              ]
+            },
+            then: "ebook"
+          },
+          {
+            case:{
+              $and: [
+                {$eq: ["$product_type", "unspecified"]},
+                {$ne: ["$cover_type", null]}
+              ]
+            },
+            then: "physical_book"
+          }
+        ],
+        default: "$product_type"
+      }
+    }
+  }
+};
+
+var merge_stage_ebook_and_physical_book = {
+  $merge: {
+    into: "books_collection",
+    on: "_id",
+    whenMatched: "replace",
+    whenNotMatched: "discard"
+  }
+};
+
+db.books_collection.aggregate([ebook_and_physicalbook, merge_stage_ebook_and_physical_book]);
 
 
-db.books_collection.find()
+
+db.books_collection.countDocuments({product_type: "physical_book"})
